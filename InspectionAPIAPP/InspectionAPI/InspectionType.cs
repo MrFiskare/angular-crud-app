0d7e2e7b712b2fbf4a63e3c0b7223f5983fa8286
Roland Halász <roland.halasz@outlook.com>
2022-05-04 14:35:20 +0200

    frontend created

---
+++ InspectionAPIAPP/InspectionAPI/InspectionType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class InspectionType
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
