ea536299d4a96613d8aa97cd6d41bb2dec847535
Roland Halász <roland.halasz@outlook.com>
2022-05-04 14:53:45 +0200

    add-edit-inspection component created

--- angular13-inspaction-api/src/app/app.module.ts
+++ angular13-inspaction-api/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InspectionComponent } from './inspection/inspection.component';
 import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
+import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InspectionComponent
+    AddEditInspectionComponent
   ],
   imports: [
     BrowserModule
